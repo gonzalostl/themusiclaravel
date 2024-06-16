5482cab89147794b030f6efa5a8424188963a2f1
gsanlop705 <gsanlop705@g.educaand.es>
2024-06-17 00:03:49 +0200

    commit final

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,7 +1,33 @@
 
 
+@extends('layouts.app2')
 
-<h1>Crear nuevo usuario</h1>
+<link rel="stylesheet" href="/css/edit.css">
+
+<body>
+<aside id="sidebar">
+
+<ul>
+    <li>
+        <img src="/imagen/logothemusic.png" class="logo" width="40%">
+    </li>
+    <li><a href="{{ route('home') }}" class="menu-item active"><i class="fas fa-home"></i> Inicio</a></li>          
+        <li><a href="{{ route('canciones.index') }}" class="menu-item"><i class="fas fa-music"></i> Canciones</a></li>
+        <li><a href="{{route('generos.index')}}" class="menu-item"><i class="fas fa-list"></i> Géneros</a></li>
+        <li><a href="{{ route('playlists.index') }}" class="menu-item"><i class="fas fa-list"></i> Playlists</a></li>
+        <li><a href="{{route('users.index')}}" class="menu-item"><i class="fas fa-user"></i> Usuarios</a></li>
+</ul>
+<ul>
+    <li>    <a href="{{route('logout')}}" class="menu-item">
+        <i class="fas fa-sign-out-alt"></i> Cerrar sesión
+    </a></li>
+</ul>
+
+
+</aside>
+<div class="content">
+    <div class="form-container">
+    <h1>Crear nuevo usuario</h1>
 
 <form method="POST" action="{{ route('users.store') }}">
     @csrf
@@ -23,3 +49,24 @@
 
     <button type="submit">Crear usuario</button>
 </form>
+    </div>
+</div>
+</body>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script>
+    $(document).ready(function (e) { 
+        $('#portada').change(function () { 
+            let reader = new FileReader();
+            reader.onload = (e) => {
+                $('#imagenSeleccionada').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(this.files[0]);
+         });
+     });
+</script>
+
+
+
+
